46cf2689ab177926f5c50552e6aa3d726aef8ac4
Yoon JiHo <dbswlgh0627@gamil.com>
2026-05-02 23:24:32 +0900

    작업 중간 저장

--- simulator/algorithm/srtn.py
+++ simulator/algorithm/srtn.py
@@ -2,7 +2,7 @@ from collections import deque
 
 from simulator.models import Core, ExecutionBlock, Process, ScheduleResult
 
-
+"""프로세스 burst를 알아야 함. 선점임. 프로세스가 새로 들어왔을 때 비교하여 더 작은 것 실행(이미 작은 게 있다면 그거랑만 비교)."""
 class SRTN:
     def __init__(self):
         self.total_energy = 0.0
